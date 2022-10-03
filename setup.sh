be13cc775643e644f0e245711982cad20d0d6c40
z3r0privacy <michi_roth@gmx.ch>
2022-10-03 07:07:23 +0200

    added commands for interface creation

--- setup.sh
+++ setup.sh
@@ -2,13 +2,15 @@
 
 # as root
 
-apt update
-apt upgrade -y
+apt-get update
+apt-get upgrade -y
 peer_keys=$(pwd)/clients_pubkeys
-apt install wireguard -y
+apt-get install wireguard -y
 cd /etc/wireguard/
 umask 077
 wg genkey | tee privatekey | wg pubkey > publickey
+ip link add dev wg0 type wireguard
+ip address add dev wg0 192.168.210.1/24
 wg set wg0 private-key /etc/wireguard/privatekey listen-port 51820
 for i in $(ls $peer_keys/*.pubkey); do echo add peer $i; wg set wg0 peer $(cat $i); done
 ip link set up dev wg0
