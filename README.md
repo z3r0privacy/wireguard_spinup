7dc944c5f37f7a089f5436ea005b73b92f5eee19
z3r0privacy <michi_roth@gmx.ch>
2022-10-03 17:01:03 +0200

    updated scripts & readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # wireguard_spinup
 Automatically setup wireguard VPN server
+
+# Deployment
+Create a virtual maschine and then exec as root:
+```bash
+cd /root
+git clone https://github.com/z3r0privacy/wireguard_spinup.git
+cd wireguard_spinup && chmod +x setup.sh
+./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# as root
-
+# system update & installation
 apt-get update
 apt-get upgrade -y
 peer_keys=$(pwd)/clients_pubkeys
 apt-get install wireguard -y
+
+# wireguard configuration
 cd /etc/wireguard/
 umask 077
 wg genkey | tee privatekey | wg pubkey > publickey
 ip link add dev wg0 type wireguard
 ip address add dev wg0 192.168.210.1/24
 wg set wg0 private-key /etc/wireguard/privatekey listen-port 51820
-for i in $(ls $peer_keys/*.pubkey); do echo add peer $i; wg set wg0 peer $(cat $i); done
+for i in $(ls $peer_keys/*.pubkey); do echo add peer $i; wg set wg0 peer $(cat $i) allowed-ips 0.0.0.0/0; done
 ip link set up dev wg0
 
+# routing & firewall configuration
+sysctl -w net.ipv4.ip_forward=1
+ufw route allow in on wg0 out on th0
+iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
+
+# show required config for clients
+
 echo Public IP: $(dig +short myip.opendns.com @resolver1.opendns.com):51820
-echo WG conf: $(wg status)
+echo WG conf: $(wg show)
